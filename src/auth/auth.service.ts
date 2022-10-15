a4925e9590c9b7333ea3bee15910c5b07ea2165d
Swapnil Jagtap <sjagtap@deqode.com>
2022-10-16 02:27:02 +0530

    feat: user signup api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SignupDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-
-  signup() {
-    return {
-      id: '20',
-      success: true,
-    };
+  async signup({ email, password }: SignupDto) {
+    const hash = await argon.hash(password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return {
+        data: user,
+        success: true,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+    }
   }
   login() {
     return {
